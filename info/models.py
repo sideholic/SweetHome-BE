91061960bcf4153f3d00cb572c7e4211c6a62c33
donghwan park <39932335+dong-park@users.noreply.github.com>
2022-03-04 21:29:36 +0900

    add news model

--- info/models.py
+++ info/models.py
@@ -6,3 +6,11 @@ class Info(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     answer = models.IntegerField()
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    createdAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
+    isDelete = models.BooleanField()
