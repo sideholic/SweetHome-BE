80f7652b3a4da5e0c45b987dc9c3594fdb0dbc94
donghwan park <39932335+dong-park@users.noreply.github.com>
2022-03-07 22:18:22 +0900

    아파트 청약 공공정보 API 정보 가져오기

--- info/subscription_api.py
+++ info/subscription_api.py
@@ -1,16 +1,28 @@
 import requests
 
-api_key = "OJZ9FrlEdT7%252BvgBrdLN4C%252FOeYhGVPV0t6VaSIUuZX6T7AqeBTFHcuyKiVf1nOD3n9FNRtbhCgBesvmVUFNdSwg%253D%253D"
+from .models import Info, Apt
+
+api_key = "tUJXMx507TgYQn2vJOL0q+WiOMNLBuJKtPRH/JPmgKdyOUtg4OZBAAlS8QKzDmLyMkhmIj9b6K0bKNULwIL0Dg=="
 api_key_dcode = requests.utils.unquote(api_key, encoding='utf-8')
+url = "https://api.odcloud.kr/api/ApplyhomeInfoDetailSvc/v1"
 
 
-def get_lttot_pblanc_list():
-    parameters = {'ServiceKey': 'tUJXMx507TgYQn2vJOL0q%252BWiOMNLBuJKtPRH%252FJPmgKdyOUtg4OZBAAlS8QKzDmLyMkhmIj9b6K0bKNULwIL0Dg%253D%253D',
-                  'startmonth': '202101', 'endmonth': '202103', 'houseSecd': '01', 'sido': '강원',
-                  'houseName': '횡성 벨라시티', 'rentSecd': '0'}
-
-    print(parameters)
-
-    url = "http://openapi.reb.or.kr/OpenAPI_ToolInstallPackage/service/rest/ApplyhomeInfoSvc/getLttotPblancList"
-    req = requests.get(url, params=parameters)
+def getApt():
+    parameters = {
+        'serviceKey': api_key,
+        'perPage': 10000,
+        # 'startmonth': '202101', 'endmonth': '202103', 'houseSecd': '01', 'sido': '강원',
+        # 'houseName': '횡성 벨라시티', 'rentSecd': '0'
+    }
+    req = requests.get(url + '/getAPTLttotPblancDetail', params=parameters)
     return req
+
+
+def saveApt(apts):
+    json_data = apts.json()['data']
+
+    for val in json_data:
+        apt = Apt().from_json(val)
+        apt.save()
+
+
