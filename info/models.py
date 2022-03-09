16e98ab5532899e3f10dfad70c88aebdfb851b7b
donghwan park <39932335+dong-park@users.noreply.github.com>
2022-03-09 18:51:37 +0900

    Urbty(오피스텔/도시형/민간임대 분양정보 모델링)

--- info/models.py
+++ info/models.py
@@ -56,7 +56,7 @@ class Apt(models.Model):
     SPSPLY_RCEPT_ENDDE = models.CharField(max_length=100, null=True)
     SUBSCRPT_AREA_CODE = models.CharField(max_length=100, null=True)
     SUBSCRPT_AREA_CODE_NM = models.CharField(max_length=100, null=True)
-    TOT_SUPLY_HSHLDCO = models.IntegerField(),
+    TOT_SUPLY_HSHLDCO = models.IntegerField()
 
     def from_json(self, json):
         self.BSNS_MBY_NM = json['BSNS_MBY_NM']
@@ -99,4 +99,52 @@ class Apt(models.Model):
         self.SUBSCRPT_AREA_CODE = json['SUBSCRPT_AREA_CODE']
         self.SUBSCRPT_AREA_CODE_NM = json['SUBSCRPT_AREA_CODE_NM']
         self.TOT_SUPLY_HSHLDCO = json['TOT_SUPLY_HSHLDCO']
+        return self
+
+
+class Urbty(models.Model):
+    BSNS_MBY_NM = models.TextField(null=True)
+    CNTRCT_CNCLS_BGNDE = models.TextField(null=True)
+    CNTRCT_CNCLS_ENDDE = models.TextField(null=True)
+    HMPG_ADRES = models.TextField(null=True)
+    HOUSE_DTL_SECD = models.TextField(null=True)
+    HOUSE_DTL_SECD_NM = models.TextField(null=True)
+    HOUSE_MANAGE_NO = models.IntegerField(null=True)
+    HOUSE_NM = models.TextField(null=True)
+    HOUSE_SECD = models.TextField(null=True)
+    HOUSE_SECD_NM = models.TextField(null=True)
+    HSSPLY_ADRES = models.TextField(null=True)
+    HSSPLY_ZIP = models.TextField(null=True)
+    MDHS_TELNO = models.TextField(null=True)
+    MVN_PREARNGE_YM = models.TextField(null=True)
+    PBLANC_NO = models.IntegerField(primary_key=True)
+    PRZWNER_PRESNATN_DE = models.TextField(null=True)
+    RCRIT_PBLANC_DE = models.TextField(null=True)
+    SEARCH_HOUSE_SECD = models.TextField(null=True)
+    SUBSCRPT_RCEPT_BGNDE = models.TextField(null=True)
+    SUBSCRPT_RCEPT_ENDDE = models.TextField(null=True)
+    TOT_SUPLY_HSHLDCO = models.IntegerField(null=True)
+
+    def from_json(self, val):
+        self.BSNS_MBY_NM = val['BSNS_MBY_NM']
+        self.CNTRCT_CNCLS_BGNDE = val['CNTRCT_CNCLS_BGNDE']
+        self.CNTRCT_CNCLS_ENDDE = val['CNTRCT_CNCLS_ENDDE']
+        self.HMPG_ADRES = val['HMPG_ADRES']
+        self.HOUSE_DTL_SECD = val['HOUSE_DTL_SECD']
+        self.HOUSE_DTL_SECD_NM = val['HOUSE_DTL_SECD_NM']
+        self.HOUSE_MANAGE_NO = val['HOUSE_MANAGE_NO']
+        self.HOUSE_NM = val['HOUSE_NM']
+        self.HOUSE_SECD = val['HOUSE_SECD']
+        self.HOUSE_SECD_NM = val['HOUSE_SECD_NM']
+        self.HSSPLY_ADRES = val['HSSPLY_ADRES']
+        self.HSSPLY_ZIP = val['HSSPLY_ZIP']
+        self.MDHS_TELNO = val['MDHS_TELNO']
+        self.MVN_PREARNGE_YM = val['MVN_PREARNGE_YM']
+        self.PBLANC_NO = val['PBLANC_NO']
+        self.PRZWNER_PRESNATN_DE = val['PRZWNER_PRESNATN_DE']
+        self.RCRIT_PBLANC_DE = val['RCRIT_PBLANC_DE']
+        self.SEARCH_HOUSE_SECD = val['SEARCH_HOUSE_SECD']
+        self.SUBSCRPT_RCEPT_BGNDE = val['SUBSCRPT_RCEPT_BGNDE']
+        self.SUBSCRPT_RCEPT_ENDDE = val['SUBSCRPT_RCEPT_ENDDE']
+        self.TOT_SUPLY_HSHLDCO = val['TOT_SUPLY_HSHLDCO']
         return self
